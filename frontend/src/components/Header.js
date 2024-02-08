6e8b337d84c6eac5de48d16447d248b2eb344136
Alex-Buis <alexandre59buisine@gmail.com>
2024-02-08 17:10:26 +0100

    🎨 Front - Skeleton Home

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header() {
   return (
     <header>
-        <h1>Header</h1>
+        <h1>Tandem</h1>
     </header>
   )
 }
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,11 +1,69 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>
-        <h1>Home</h1>
-    </div>
-  )
+    return (
+        <div>
+            <div>
+
+                <h1>Faites germer vos projets</h1>
+                <h2>bouton crée un projet + route</h2>
+
+            </div>
+            <div>
+
+                <p>Barre de recherche</p>
+                <p>Filtre x3 </p>
+
+            </div>
+            <div>
+
+                <h1>Liste des projets</h1>
+                <p>Projet 1</p>
+                <p>Projet 2</p>
+
+            </div>
+            <div>
+                <h1>Les coups de coeur + carousel de carte crée</h1>
+                <div>
+                    <p>Img</p>
+                    <p>Titre</p>
+                    <p>Nom</p>
+                </div>
+                <div>
+                    <p>Description</p>
+                    <p>Text</p>
+                </div>
+            </div>
+            <div>
+                <div>
+
+                <h1>Recommandations</h1>
+                <p>Img</p>
+                <p>Titre</p>
+                <p>Text</p>
+                <p>Language + Nom créateur + Nombre + logo</p>
+                </div>
+                <div>
+                <h1>Recommandations 2 </h1>
+                <p>Img</p>
+                <p>Titre</p>
+                <p>Text</p>
+                <p>Language + Nom créateur + Nombre + logo</p>
+                </div>
+                <div>
+                    <p>En voir plus</p>
+                </div>
+                <div>
+                    <h1>Les créateurs les plus actifs</h1>
+                    <div>
+                        <p>Liste des profil</p>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    )
 }
 
 export default Home
