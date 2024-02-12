0865110c6082c87ebd28a4b16fcc1881ccac03dd
Morgan-B3 <boutroism@gmail.com>
2024-02-12 15:32:14 +0100

    ✅ FRONT - added Login route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import './stylesheets/App.scss';
 import Home from './pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
     </BrowserRouter>
   );
