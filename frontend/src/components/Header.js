05bb8eca8cd651234c3c645e07433642bf60fa45
Morgan-B3 <boutroism@gmail.com>
2024-02-13 11:28:55 +0100

    🎨 ✅ FRONT - added navigate to Home and Login in header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Header() {
+  const navigate = useNavigate();
+  
   return (
     <header>
-        <h1>Tandem</h1>
+        <h1 onClick={()=>navigate('/')}>Tandem</h1>
+        <p onClick={()=>navigate('/login')}>Connexion</p>
     </header>
   )
 }
