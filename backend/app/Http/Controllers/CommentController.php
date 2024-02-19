0a7c466dd55c422331cb7b5f5593de6eb158d66a
Morgan-B3 <boutroism@gmail.com>
2024-02-19 16:29:50 +0100

    💡 BACK - updated comments in CommentController

--- backend/app/Http/Controllers/CommentController.php
+++ backend/app/Http/Controllers/CommentController.php
@@ -9,29 +9,33 @@ use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
+    /**
+     * Affiche les commentaires d'un projet
+     */
     public function show($id){
         $project = Project::find($id);
-        $comments = $project->comment()->get();
-        $commentsList = [];
+        $comments = $project->comments()->get();
+        // Attribution de l'utilisateur et de son avatar pour chaque commentaire du projet
         foreach($comments as $comment){
-            array_push($commentsList, [
-                'id' => $comment->id,
-                'content' => $comment->comment,
-                'user' => $comment->user()->get(),
-                'admin' => $project->admin,
-                'date' => $project->created_at,
-                'comment' => $project->comment_id ?? null,
-            ]);
+            $user = $comment->user()->first();
+            $avatar = $user->avatar()->first();
+            $user->avatar = $avatar;
+            $comment->user = $user;
         }
         return response()->json([
             'comments' => $comments
         ]);
     }
 
+    /**
+     * Création d'un nouveau commentaire / Réponse à un commentaire
+     * 
+     * @param string $request Contenu du commentaire
+     * @param int $project_id ID du projet commenté
+     * @param int $comment_id ID du commentaire auquel celui-ci répond (facultatif)
+     */
     public function create(Request $request, $project_id, $comment_id = null){
-        $validator = Validator::make($request->all(),[
-            'content' => "min:3|max:1000",
-        ]);
+        $validator = Validator::make($request, "min:3|max:1000");
         if($validator->fails()){
             return response()->json([
                 'errors' => $validator->messages(),
@@ -39,13 +43,13 @@ class CommentController extends Controller
             ]);
         } else {
             $comment = new Comment();
-            $comment->content = $request->content;
+            $comment->content = $request;
             $comment->user_id = auth()->user()->id;
             $comment->comment_id = $comment_id;
             $comment->project_id = $project_id;
 
             $project = Project::find($project_id);
-            if($project->user_id === auth()->user()->id){
+            if($project->user_id == auth()->user()->id){
                 $comment->admin = true;
             } else {
                 $comment->admin = false;
@@ -59,20 +63,23 @@ class CommentController extends Controller
         }
     }
 
+    /**
+     * Mise à jour d'un commentaire
+     * 
+     * @param string $request Contenu du commentaire
+     */
     public function update(Request $request, $id){
         $comment = Comment::find($id);
-        if($comment->user_id === auth()->user()->id){
-            $validator = Validator::make($request->all(),[
-                'content' => "min:3|max:1000",
-            ]);
+        // Autorisation seulement à l'auteur du commentaire
+        if($comment->user_id == auth()->user()->id){
+            $validator = Validator::make($request,"min:3|max:1000");
             if($validator->fails()){
                 return response()->json([
                     'errors' => $validator->messages(),
                     'status' => "error"
                 ]);
             } else {
-                
-                $comment->content = $request->content;
+                $comment->content = $request;
                 $comment->save();
                 
                 return response()->json([
@@ -86,10 +93,14 @@ class CommentController extends Controller
         }
     }
 
+    /**
+     * Suppression d'un commentaire
+     */
     public function delete($id){
         $comment = Comment::find($id);
         $admin_id = $comment->project()->get()->user_id;
-        if($admin_id === auth()->user()->id || $comment->user_id === auth()->user()->id ){
+        // Autorisation seulement à l'auteur du commentaire ou au créateur du projet
+        if($admin_id == auth()->user()->id || $comment->user_id === auth()->user()->id ){
             Comment::destroy($id);
 
             return response()->json([
