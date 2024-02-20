fdd9b5d094f9fd06cab3cff7aaaed2d39a7bf7e9
mohamede <razzouki.mohamed12@gmail.com>
2024-02-20 16:04:08 +0100

    Front-Menu-Burger-M

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,26 @@
-import "../stylesheets/style.css";
-import React, { useEffect, useState } from 'react'
+import "../stylesheets/MenuBurger.scss";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuthContext from "../context/AuthContext";
 //import axios from '../api/axios';
 
+
 function Header() {
 
+  const [showTiret, setShowTiret] = useState(false)
+  const handleShowTiret = () => {
+    setShowTiret(!showTiret)
+  }
 
+  const changeRoute = (path) => {
+    handleShowTiret()
+    navigate(path)
+  }
   // const {user, getUser} = useAuthContext();
 
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("user")) || null);
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("user")) || null
+  );
   //const [token, setToken] = useState(JSON.parse(localStorage.getItem("token")) || null);
 
   // useEffect(()=> {
@@ -17,7 +28,6 @@ function Header() {
   //     getUser();
   //   }
   // }, []);
-
 
   // const getUser = async () => {
   //   const {data} = await axios.get(`/api/user`, {headers:{"Authorization":`Bearer ${token}`}});
@@ -27,21 +37,35 @@ function Header() {
   const navigate = useNavigate();
 
   return (
-    <header id="nav" className="active">
-          <h1 className="tandem" onClick={() => navigate("/")}>
-            Tandem
-          </h1>
-      <ul>
+    <header className={`navbar ${showTiret ? "show-nav" : "false"} `}>
+      <h1 className="tandem" onClick={() => changeRoute("/")}>
+        Tandem
+      </h1>
+      <ul className="all-item">
         {user ? (
-          <li onClick={() => navigate("/logout")}>{user.name}</li>
+          <li className="item" onClick={() => changeRoute("/logout")}>
+            <p className="item-p">{user.name}</p>
+          </li>
         ) : (
-          <li onClick={() => navigate("/login")}>Connexion</li>
+          <li className="item slideInDown-1"  onClick={() => changeRoute("/login")}>
+            <p className="item-p">Connexion</p>
+          </li>
         )}
-        <li onClick={() => navigate("/create")}>Créer un projet</li>
-        <li>Recherche</li>
-        <li>Notifications</li>
+        <li className="item slideInDown-2" onClick={() => changeRoute("/create")}>
+          <p className="item-p">Créer un projet</p>
+        </li>
+
+        <li className="item slideInDown-3" >
+          <p className="item-p">Recherche</p>
+        </li>
+
+        <li className="item slideInDown-4" >
+          <p className="item-p">Notifications</p>
+        </li>
       </ul>
-      <div id="icons" ></div>
+      <button className="separation" onClick={handleShowTiret}>
+        <span className="tiret"></span>
+      </button>
     </header>
   );
 }
