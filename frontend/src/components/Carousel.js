61ddb7da64a7794c7518bd31d13893ec7e742af9
Alex-Buis <alexandre59buisine@gmail.com>
2024-02-22 14:40:17 +0100

    ✅ 💄  Front - SASS - ADD Creator in carousel

--- frontend/src/components/Carousel.js
+++ frontend/src/components/Carousel.js
@@ -1,13 +1,16 @@
-import { Carousel as AntdCarousel } from 'antd';
+import { Carousel as AntdCarousel, Popover, Progress } from 'antd';
 import "../stylesheets/Carousel.scss";
 import { useEffect, useState } from "react";
 import Project from './Project';
 import { Navigate, useNavigate } from 'react-router-dom';
+import axios from '../api/axios';
+import { LuUser2 } from 'react-icons/lu';
 
 
-const Carousel = () => {
+const Carousel = ({ creator_id, collaborators, collaborators_max }) => {
     const [projects, setProjects] = useState([]);
     const navigate = useNavigate();
+    console.log()
     const getProjects = async () => {
         const data = await fetch("http://127.0.0.1:8000/api/projects").then((res) => res.json()
         );
@@ -27,6 +30,12 @@ const Carousel = () => {
         textAlign: 'center',
         display: 'flex',
     };
+    const colors = {
+        '0%': '#2EC458',
+        '50%': '#ADDDB2',
+        // '60%': '#FFD7B4',
+        '100%': '#F47143'
+    }
 
     console.log(projects);
     const projectscarouel = projects.slice(0, 1).map(project => {
@@ -41,11 +50,19 @@ const Carousel = () => {
                             <h2>{project.title}</h2>
                             <h5 className='creator-carousel'>{project.creator.name}</h5>
                         </div>
-                        <div className='project-body project-body-carousel'>
-                            <p className='description'>{project.description}</p>
+                        <div className='project-body-between'>
+
+                            <div className='project-body project-body-carousel'>
+                                <p className='description'>{project.description}</p>
+                                <Popover placement="left" content={project.collaborators === project.collaborators_max ? "Equipe complète" : ` ${project.collaborators_max - project.collaborators} place(s) restante(s)`}>
+                                    <div className='progress'>
+                                        <LuUser2 size={30} color={project.collaborators === project.collaborators_max ? '#F47143' : '#2EC458'} />
+                                        <Progress className={project.collaborators === project.collaborators_max ? 'orange' : 'green'} type='circle' percent={(project.collaborators / project.collaborators_max) * 100} size="small" format={(percent) => `${project.collaborators}/${project.collaborators_max}`} strokeColor={project.collaborators === project.collaborators_max ? '#F47143' : colors} />
+                                    </div>
+                                </Popover>
+                            </div>
                         </div>
                     </div>
-
                 </div>
             </div>
         );
