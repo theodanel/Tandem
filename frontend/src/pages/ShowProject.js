b305b5bb34985578db7416534169e634242f02ce
theodanel <theodanel@gmail.com>
2024-02-26 15:57:11 +0100

    FRONT _ Displaying all languages when updating project

--- frontend/src/pages/ShowProject.js
+++ frontend/src/pages/ShowProject.js
@@ -1,7 +1,7 @@
 
 import axios from '../api/axios';
-import React, {Fragment, useEffect, useState} from 'react'
-import {Modal, Skeleton, Steps} from "antd"
+import React, { Fragment, useEffect, useState } from 'react'
+import { Collapse, Modal, Skeleton, Steps } from "antd"
 import { useNavigate, useParams } from 'react-router-dom'
 import Layout from '../components/Layout';
 
@@ -9,6 +9,7 @@ import Layout from '../components/Layout';
 import Language from '../components/Language.js';
 import { PiTreeLight, PiPlantLight } from "react-icons/pi";
 import { LuNut } from "react-icons/lu";
+import { format } from "date-fns";
 
 
 import "../stylesheets/ProjectDetail.scss"
@@ -24,30 +25,119 @@ const ShowProject = () => {
     const [newDescription, setNewDescription] = useState("");
     const [postComment, setPostComment] = useState([]);
     const [newCollaborators, setNewCollaborators] = useState("");
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState({
+        user: true,
+        languages: true,
+    });
+    const [allLanguages, setAllLanguages] = useState([]);
+
+
+    const [checkedState, setCheckedState] = useState([]);
 
     const navigate = useNavigate();
 
     const token = useSelector(state => state.data.token);
 
-    const getProject = async()=> {
+    const getProject = async () => {
         const res = await axios.get(`/api/project/${id}`)
-        Object.keys(res.data.project).map((key, index)=>{
-            setProject(project => ({...project, [key]:res.data.project[key]}));
+        Object.keys(res.data.project).map((key, index) => {
+            setProject(project => ({ ...project, [key]: res.data.project[key] }));
         })
         document.title = `${res.data.project.title}`;
         setLoading(false);
     };
 
 
-    const languagesList = project.languages?.map((language) => {
+
+    // Ajout / suppression d'un langage selon son état checked
+    const handleLanguage = (languageId) => {
+        // Mise à jour de la valeur du checked du langage sélectionné (true devient false et vice versa)
+        const updatedCheckedState = checkedState.map((item, index) =>
+            // parcourt le tableau et ne change que le langage selectionné
+            index === languageId - 1 ? !item : item
+        );
+
+
+
+
+        // Mise à jour du tableau checked avec cette nouvelle donnée
+        setCheckedState(updatedCheckedState);
+
+        // 2 actions possible selon l'état checked :
+        if (updatedCheckedState[languageId - 1]) {
+            // si checked : true
+            // Ajout de l'id du langage dans le tableau project.languages
+            setProject({
+                ...project,
+                languages: [...project.languages, languageId],
+            });
+        } else {
+            // si checked : false
+            // Filtre le tableau project.languages pour retirer l'id du langage
+            setProject({
+                ...project,
+                languages: [...project.languages.filter((id) => id !== languageId)],
+            });
+        }
+    };
+
+
+    const getLanguages = async () => {
+        
+            const resLanguages = await axios("/api/languages").then(
+                (res) => res.data.languages
+            );
+            setAllLanguages(resLanguages);
+            // remplissage conditionnel tu tableau d'etat checked pour la liste des langages
+            resLanguages.map((language) => {
+                if (
+                    project.languages.find(
+                        (projectLanguage) => projectLanguage.id === language.id
+                    )
+                ) {
+                    setCheckedState((checkedState) => [...checkedState, true]);
+                } else {
+                    setCheckedState((checkedState) => [...checkedState, false]);
+                }
+            });
+        
+    };
+
+
+    const getData = async () => {
+        const resProjects = await axios
+            .get(`/api/projects/${id}`)
+            .then((res) => res.data.projects);
+        setProject(resProjects);
+
+
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+
+    const languagesList = project.languages?.map((language, index) => {
         return (
-            <Language key={language.id}
+            <Language key={language.id} name={language.name} image={language.logo} />
+        );
+    });
+
+
+    const allLanguagesList = allLanguages.map((language, index) => {
+        return (
+            <Language
+                key={language.id}
                 name={language.name}
+                checked={checkedState[index]}
+                action={() => handleLanguage(language.id)}
                 image={language.logo}
+                type="checkbox"
             />
         );
     });
+
 
 
     useEffect(() => {
@@ -83,9 +173,9 @@ const ShowProject = () => {
 
     const update = async (e) => {
         e.preventDefault();
-        const res = await axios.put(`/api/project/${id}/update`, {newTitle, newDescription, newCollaborators},{headers:{"Authorization":`Bearer ${token}`}});
+        const res = await axios.put(`/api/project/${id}/update`, { newTitle, newDescription, newCollaborators }, { headers: { "Authorization": `Bearer ${token}` } });
 
-        
+
         if (res.data.status === 200) {
             setNewTitle("");
             setNewDescription("");
@@ -99,8 +189,8 @@ const ShowProject = () => {
 
     }
 
-    const changeStatus = async(statusType) => {
-        const res = await axios.put(`api/project/${id}/step`, {status: statusType} ,{headers:{"Authorization":`Bearer ${token}`}})
+    const changeStatus = async (statusType) => {
+        const res = await axios.put(`api/project/${id}/step`, { status: statusType }, { headers: { "Authorization": `Bearer ${token}` } })
 
         setStatus(project.status)
         // if(project.status === "created") {
@@ -111,7 +201,7 @@ const ShowProject = () => {
     }
 
     // Permet d'afficher la liste des collaborateurs faisant partie d'un projet
-    const collaboratorsList = project.collaboratorsList?.map((collaborator, index) => {
+    const collaboratorsList = project.collaboratorsList?.map((collaborator) => {
         return (
             <div className='collaboratorCard' key={collaborator.id}>
                 <img src={`http://localhost:8000/images/avatars/${collaborator.avatar.url}`} />
@@ -129,18 +219,19 @@ const ShowProject = () => {
     });
 
 
+
     const handleChange = (e) => {
         setPostComment({
             ...postComment,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
 
     const postComments = async () => {
-        const res = await axios.post(`/api/comment/${id}/store`, {comment: postComment.comments}, {headers:{"Authorization":`Bearer ${token}`}});
+        const res = await axios.post(`/api/comment/${id}/store`, { comment: postComment.comments }, { headers: { "Authorization": `Bearer ${token}` } });
 
-        
+
         if (res.data.status === 200) {
             setPostComment(project.comments)
         } else {
@@ -156,9 +247,8 @@ const ShowProject = () => {
 
     return (
         <Layout>
-            <Skeleton loading={loading} active>
 
-           
+
             <div className='projectDetail'>
                 <div className='projectDetailPosition'>
                     <div className='imagePosition'>
@@ -173,11 +263,11 @@ const ShowProject = () => {
                                 <hr className='titleDecoration'></hr>
                             </div>
                             <div className='creator'>
-                                <h4 >Simtouflo_59</h4>
-                                <h5> - {project.created_at}</h5>
+                                <h4 >{project.creator?.name}</h4>
+                                <h5> {project.created_at ? format(project.created_at, "dd/MM/yyyy") : ""}</h5>
                             </div>
                         </div>
-      
+
 
                         <p className='projectDescription'>{project.description}</p>
 
@@ -192,32 +282,32 @@ const ShowProject = () => {
                             </legend>
                             <hr className='languagesDecoration'></hr>
                         </div>
-                    
 
-                       
-                            <Steps direction="vertical"
-                                className='projectSteps'
-                                size="small"
-                                current={project.status}
-                                id="Steps"
-                                items={[
-                                    {
 
-                                        description: <button style={{'backgroundColor': status === 'created' ? '#77DD79' : '#77dd79a9' }} onClick={() => showModalSteps()} name='ongoing' className='stepOne'>Démarrer le projet</button>, icon: <LuNut className='stepsIcons'/>,
 
-                                    },
-                                    {
-                                        description: <button style={{'backgroundColor': status != 'ongoing'  ? '#77dd79a9' : '#77DD79' }} onClick={() => showModalSteps()} name='completed' className='stepTwo'>Projet en cours</button>, icon: <PiPlantLight className='stepsIcons'/>,
+                        <Steps direction="vertical"
+                            className='projectSteps'
+                            size="small"
+                            current={project.status}
+                            id="Steps"
+                            items={[
+                                {
 
-                                    },
-                                    {
-                                        description: <button style={{'backgroundColor': status != 'completed' ?'#f47243b4' : '#f47243'}} className='stepThree'>Projet terminé</button>, icon: <PiTreeLight className='stepsIcons'/>,
+                                    description: <button style={{ 'backgroundColor': status === 'created' ? '#77DD79' : '#77dd79a9' }} onClick={() => showModalSteps()} name='ongoing' className='stepOne'>Démarrer le projet</button>, icon: <LuNut className='stepsIcons' />,
 
-                                    },
-                                ]}
-                            />
+                                },
+                                {
+                                    description: <button style={{ 'backgroundColor': status != 'ongoing' ? '#77dd79a9' : '#77DD79' }} onClick={() => showModalSteps()} name='completed' className='stepTwo'>Projet en cours</button>, icon: <PiPlantLight className='stepsIcons' />,
 
-                        
+                                },
+                                {
+                                    description: <button style={{ 'backgroundColor': status != 'completed' ? '#f47243b4' : '#f47243' }} className='stepThree'>Projet terminé</button>, icon: <PiTreeLight className='stepsIcons' />,
+
+                                },
+                            ]}
+                        />
+
+
                     </div>
 
 
@@ -241,8 +331,8 @@ const ShowProject = () => {
                     <div id='collaborators'>
                         <h3>Laisser un commentaire</h3>
                         <div className='addComment'>
-                            <textarea onChange={(e)=> handleChange(e)}  placeholder='Lorem ipsum dolor' name='comments' id='comments' />
-                            <button type='button'  className="commentButton" onClick={() => postComments()}>Poster</button>
+                            <textarea onChange={(e) => handleChange(e)} placeholder='Lorem ipsum dolor' name='comments' id='comments' />
+                            <button type='button' className="commentButton" onClick={() => postComments()}>Poster</button>
                         </div>
                         <hr className='languagesDecoration'></hr>
 
@@ -257,13 +347,13 @@ const ShowProject = () => {
 
             <Modal title="" open={isModalStepsOpen} onCancel={handleCancel2} footer={null} centered >
                 <h3>Etes vous sur de vouloir passer à l'étape suivante ? </h3>
-                <button type='button' onClick={() => (changeStatus(), setIsModalStepsOpen(false))}  name='ongoing' className='stepOne'>Oui</button> 
-                <button type='button' onClick={() => setIsModalStepsOpen(false)}  name='closeModal' className='closeModal'>Non</button> 
-                
+                <button type='button' onClick={() => (changeStatus(), setIsModalStepsOpen(false))} name='ongoing' className='stepOne'>Oui</button>
+                <button type='button' onClick={() => setIsModalStepsOpen(false)} name='closeModal' className='closeModal'>Non</button>
+
             </Modal>
 
 
-            <Modal title="Modifier" open={isModalOpen} onCancel={handleCancel} footer={null} centered >
+            <Modal className='updateModal' title="Modifier" open={isModalOpen} onCancel={handleCancel} footer={null} centered >
                 <form onSubmit={(e) => update(e)}>
                     <input type='text' id='newTitle' name='newTitle' value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
                     <b>{errors.newTitle}</b>
@@ -271,13 +361,27 @@ const ShowProject = () => {
                     <input type='text' id='newDescription' name='newDescription' value={newDescription} onChange={(e) => setNewDescription(e.target.value)} />
                     <b>{errors.newDescription}</b>
 
-                    {/* <input type="number" id="newCollaborators" name="newCollaborators" min="1" max="20" value={newCollaborators} onChange={(e) => setNewCollaborators(e.target.value)} />
-                    <b>{errors.newCollaborators}</b> */}
+                    <Collapse
+                        onChange={() => {
+                            getLanguages();
+                        }}
+                        items={[
+                            {
+                                label: "Langages",
+                                children: (
+                                    <Skeleton loading={loading.languages} active>
+                                        <div className="updateLanguagesList">{allLanguagesList}</div>
+                                    </Skeleton>
+                                ),
+                            },
+                        ]}
+                    />
 
-                    <input type='submit' value={"Valider"}/>
+
+                    <button type="submit" className="btn-green center" />
                 </form>
             </Modal>
-            </Skeleton>
+
         </Layout>
     )
 
